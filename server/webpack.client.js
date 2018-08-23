4dbf4d05ec1efeb446fdf0564cc30f59e48d79cb
James Ko <james.ko@James-Kos-iMac.local>
2018-08-23 11:23:51 -0700

    created client side bundle that is public

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -2,15 +2,15 @@ const path = require('path');
 
 module.exports = {
 	// inform webpack that we are building a bundle for nodeJS, not for browser. default is browser.
-	target: 'node',
+	// target: 'node',
 
 	// tell webpack the root file of our server application.
-	entry: './src/index.js',
+	entry: './src/client/client.js', // usually client/index.js
 
 	// tell webpack where to put the output file that is generated.
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'build')
+		path: path.resolve(__dirname, 'public') // this is public for the client
 	},
 
 	// tell webpack to run babel on every file it runs... specifically .js files (regex) under 'test' 
